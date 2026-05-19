c1a650f33c484e5fbbb266940b8392a0646f6073
edzordzinam44 <edzordzinam.alipui@gmail.com>
2026-05-19 10:01:47 +0000

    Add "Back to Top" button functionality and styles

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const backToTopBtn = document.getElementById("backToTop");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  };
+
+  backToTopBtn.onclick = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+});
